e104f8c80a2b9e87353ff5caf2c16131d4c5d6c5
Evn Sey <evn.seyed@gmail.com>
2020-08-02 20:35:56 +0300

    Clean common webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -9,12 +9,6 @@ module.exports = {
                 exclude: /(node_modules)/,
                 use: "ts-loader",
             },
-            // {
-            //     test: /\.(js|jsx)$/,
-            //     exclude: /(node_modules|bower_components)/,
-            //     loader: "babel-loader",
-            //     options: { presets: ["@babel/env"] }
-            // },
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
